ef02de233a8beaea5fc88242cb408eb897d72886
danschoch <daniel.alexander.schoch@gmail.com>
2019-02-01 10:06:57 -0800

    add goal presentational component and move goals container to new directory

---
+++ client/src/components/Goal.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Goal = props => <div>{props.goal}</div>
+
+export default Goal;
---
+++ client/src/containers/dashboard/Goals.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Goal from '../components/Goal'
+
+class Goals extends Component {
+
+    renderGoals = () => {
+        return ['Goal 1', 'Goal 2', 'Goal 3'].map( goal => {
+            return <Goal goal={goal} />
+        })
+    }
+
+    render() {
+        return(
+           <div>
+               <h1>Goals</h1>
+               {this.renderGoals()}
+           </div>
+        )
+    }
+}
+
+export default Goals
