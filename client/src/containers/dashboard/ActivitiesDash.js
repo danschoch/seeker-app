c8dedc80b87cd5503aa71fdbd5ab64f0c148b730
danschoch <daniel.alexander.schoch@gmail.com>
2019-02-01 10:25:58 -0800

    add subheadlines to activitiesDash

--- client/src/containers/dashboard/ActivitiesDash.js
+++ client/src/containers/dashboard/ActivitiesDash.js
@@ -5,7 +5,9 @@ class ActivitiesDash extends Component {
     render() {
         return(
            <div>
-               <h1>Activities</h1>
+                <h1>Activities</h1>
+                <h2>Today</h2>
+                <h2>This Week</h2>
            </div>
         )
     }
