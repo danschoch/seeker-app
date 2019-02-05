57881440220827cc5883f2eb0605fef23e8572aa
danschoch <daniel.alexander.schoch@gmail.com>
2019-02-05 13:06:25 -0800

    add json response helper

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,13 @@
 class ContactsController < ApplicationController
+    def index
+        @contacts = Contact.all
+        json_response(@contacts)
+    end
+
+    private
+
+    def contacts_params
+        params.permit(:)
+
+
 end
