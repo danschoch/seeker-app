2f6343e3fef6433a2992e3d6631828b330797388
danschoch <daniel.alexander.schoch@gmail.com>
2019-02-05 12:22:53 -0800

    delete foreign key references in create_contacts migration (they were never necessary)

--- db/migrate/20190205193343_create_contacts.rb
+++ db/migrate/20190205193343_create_contacts.rb
@@ -8,13 +8,8 @@ class CreateContacts < ActiveRecord::Migration[5.2]
       t.string :website
       t.string :linkedin
       t.references :organization, foreign_key: true
-      t.references :last_interaction
-      t.references :next_interaction
 
       t.timestamps
     end
-
-    add_foreign_key :contacts, :contacts_activities, column: :last_interaction_id, primary_key: :activity_id
-    add_foreign_key :contacts, :contacts_activities, column: :next_interaction_id, primary_key: :activity_id
   end
 end
