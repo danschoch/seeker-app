ed29ad164c5a04124093ebbe579ee4fe7a5ab33f
danschoch <daniel.alexander.schoch@gmail.com>
2019-02-05 13:52:04 -0800

    add otehr controller actions to contacts controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,13 +1,34 @@
 class ContactsController < ApplicationController
+    before_action :set_contact, only: [:show, :update, :destroy]
+
     def index
         @contacts = Contact.all
         json_response(@contacts)
     end
 
+    def create
+        @contact = Contact.create!(contact_params)
+        json_response(@contact, :created)
+    end
+
+    def show
+        json_response(@contact)
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
     private
 
-    def contacts_params
+    def contact_params
         params.permit(:first_name, :last_name, :phone_number, :email, :website, :linkedin)
+    end
+
+    def set_contact
+        @contact = Contact.find(params[:id])
     end
 
 end
